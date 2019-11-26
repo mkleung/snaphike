755c3c922898b407d5d809a1b38c72eeec0ebd72
Mike <mike@Mikes-Mac-mini.local>
2019-11-26 15:46:26 -0500

    Add header button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import HeaderButton from '../components/HeaderButton';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
+import { Platform } from 'react-native'
+
 const HomeScreen = props => {
     return (
         <View style={styles.container}>
@@ -7,11 +11,25 @@ const HomeScreen = props => {
         </View>
     );
 };
+
+// NAVIGATION
 HomeScreen.navigationOptions = navData => {
-    return {
-        headerTitle: "Home"
-    };
+    return {
+        headerTitle: 'HomeScreen ',
+        headerRight: 
+            (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="About"
+                    iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                    onPress={() => {
+                        navData.navigation.navigate("About")
+                    }}
+                />
+                </HeaderButtons>
+            )
+    }
 };
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
