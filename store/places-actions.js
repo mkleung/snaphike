5bb7885ad5630a7bedc11585347b9fbc26cd0104
mike <mpakleung@gmail.com>
2019-12-05 16:33:28 -0500

    Add native file system

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,15 +1,36 @@
+import * as FileSystem from 'expo-file-system'
+
+
 export const ADD_PLACE = 'ADD_PLACE';
+
 let previousId = 0;
+
 export const addPlace = (title, location, image) => {
     let id = previousId + 1;
     previousId = id;
-    return {
-        type: ADD_PLACE,
-        placeData: {
-            id: id,
-            title: title,
-            location: location,
-            image: image
-        }
+
+    return async dispatch => {
+
+        // Get filename fr0m image .e.g. myimage.jpg
+        // Then create a new path
+        const newPath = FileSystem.documentDirectory + image.split('/').pop();;
+
+        // The code below takes exinsting image path and
+        // Saves it into the new path on filesystem
+        try {
+            await FileSystem.moveAsync({ from: image, to: newPath })
+        } catch (err) { console.log(err) }
+
+        // Reducer ADD_PLACE (adds an item to store)
+        dispatch({
+            type: ADD_PLACE,
+            placeData: {
+                id: id,
+                title: title,
+                location: location,
+                image: newPath
+            }
+        })
+
     }
 };
